748f9480261444bec775791cbc3948a5046e26ce
RyuunosukeDS3 <matheusmiziara123@gmail.com>
2025-05-26 22:41:42 +0000

    :test_tube: test: fixing tests

--- karma.conf.js
+++ karma.conf.js
@@ -1,3 +1,6 @@
+// Karma configuration file, see link for more information
+// https://karma-runner.github.io/1.0/config/configuration-file.html
+
 module.exports = function (config) {
   config.set({
     basePath: "",
@@ -10,22 +13,22 @@ module.exports = function (config) {
       require("@angular-devkit/build-angular/plugins/karma"),
     ],
     client: {
-      jasmine: {},
-      clearContext: false,
+      jasmine: {
+        // you can add configuration options for Jasmine here
+        // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
+        // for example, you can disable the random execution with `random: false`
+        // or set a specific seed with `seed: 4321`
+      },
     },
     jasmineHtmlReporter: {
-      suppressAll: true,
+      suppressAll: true, // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require("path").join(__dirname, "./coverage/your-project-name"),
+      dir: require("path").join(__dirname, "./coverage"),
       subdir: ".",
-      reporters: [{ type: "html" }, { type: "text-summary" }],
+      reporters: [{ type: "lcov" }],
     },
-    reporters: ["progress", "kjhtml"],
-    port: 9876,
-    colors: true,
-    logLevel: config.LOG_INFO,
-    autoWatch: true,
+    reporters: ["progress", "kjhtml", "coverage"],
     browsers: ["ChromeHeadlessNoSandbox"],
     customLaunchers: {
       ChromeHeadlessNoSandbox: {
@@ -33,7 +36,6 @@ module.exports = function (config) {
         flags: ["--no-sandbox", "--disable-setuid-sandbox"],
       },
     },
-    singleRun: false,
     restartOnFileChange: true,
   });
 };
