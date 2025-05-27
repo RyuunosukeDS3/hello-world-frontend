030ad6e6b57555e503fea5603907e06c244e5cf3
RyuunosukeDS3 <matheusmiziara123@gmail.com>
2025-05-27 03:03:06 +0000

    :bug: fix: using envs properly

--- src/app/features/hello/hello.service.ts
+++ src/app/features/hello/hello.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
@@ -9,6 +10,6 @@ export class HelloService {
   constructor(private readonly http: HttpClient) {}
 
   getMessage(): Observable<{ message: string }> {
-    return this.http.get<{ message: string }>('http://localhost:3000/hello');
+    return this.http.get<{ message: string }>(`${environment.apiUrl}/hello`);
   }
 }
